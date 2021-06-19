7e7c247bde406666d1a88ec700dcee66b7d10117
Ross Hutchison <ross-hutchison-uni@virginmedia.com>
2021-06-19 18:26:28 +0100

    refined editorControl to use the new mwthods added into editor.c making it much more understandable and reducing repeated code

--- src/editor/editorControl.c
+++ src/editor/editorControl.c
@@ -1,37 +1,20 @@
 #include "editor.h"
 
 int main() {
-    char active = TRUE;
-    uint8_t count = 0;
     char res;
-    startup();
-    while(active) {
+    editor_t *editor = new_editor(10,10);
+    startup(editor);
+
+    while(TRUE) {
         char inpt = '\0';
         res = read(STDIN_FILENO, &inpt, 1);
         if(res > 0) {
-            char* back = (char*)calloc(3 + MAX_LINE_NUM_SIZE, sizeof(char));
-            write(STDOUT_FILENO, &inpt, 1);
-            count++;
             if(inpt == '.') {
-                write(STDOUT_FILENO, "\x1b[1D", 4);
-                write(STDOUT_FILENO, "\x1b[0K", 4);
-                write(STDOUT_FILENO, "\n", 1);
-                sprintf(back, "\x1b[%dD", count);
-                write(STDOUT_FILENO, back, strlen(back));
+                clearWhole(editor);
                 break;
             }
-            if(inpt == ',') {
-                sprintf(back, "\x1b[%dD", count);
-                write(STDOUT_FILENO, back, strlen(back));
-                write(STDOUT_FILENO, "\x1b[2K", 4);
-                count = 0;
-            }
-            if(count == MAX_LINE_L) {
-                write(STDOUT_FILENO, "\n", 1);
-                sprintf(back, "\x1b[%dD", MAX_LINE_L);
-                write(STDOUT_FILENO, back, strlen(back));
-                count = 0;
-            }
+            else if(inpt == ',') clearLine(editor);
+            else writeChar(editor, inpt);
         }
     }
     return 0;
